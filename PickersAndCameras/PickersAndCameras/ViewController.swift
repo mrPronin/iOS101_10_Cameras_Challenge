2ffd3676662cfe1f698de1fb4114166f8fd5a27b
Alexander Pronin <pronin.alx@gmail.com>
2016-01-17 17:31:15 +0200

    Final commit

--- PickersAndCameras/PickersAndCameras/ViewController.swift
+++ PickersAndCameras/PickersAndCameras/ViewController.swift
@@ -9,60 +9,78 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-  @IBOutlet weak var imageView: UIImageView!
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-  
-  @IBAction func pickFromCamera(sender: AnyObject) {
     
-  }
-  
-  @IBAction func saveImage(sender:AnyObject) {
+    //MARK: - Outlets
+    @IBOutlet weak var imageView: UIImageView!
     
-  }
-  
-  @IBAction func pickFromCameraRoll(sender: AnyObject) {
+    //MARK: - Vars
     
-
-    
-  }
-
-  @IBAction func makeNostalgic(sender: AnyObject) {
-      
-    let beginImage = CIImage(image: imageView.image!)
-    if let filter = CIFilter(name: "CISepiaTone") {
-      filter.setValue(beginImage, forKey: kCIInputImageKey)
-      filter.setValue(0.8, forKey: kCIInputIntensityKey)
-      if let outputImage = filter.outputImage {
-        
-
-        let ciContext = CIContext(options: nil)
-        let cgImageRef = ciContext.createCGImage(outputImage, fromRect:outputImage.extent)
-        let newImage = UIImage(CGImage: cgImageRef)
-        imageView.image = newImage
-        
-
-      }
-      
-
+    //MARK: - UIViewController
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
     }
-  }
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: - Actions
+    @IBAction func pickFromCamera(sender: AnyObject) {
+        if UIImagePickerController.isSourceTypeAvailable(.Camera) {
+            let picker = UIImagePickerController()
+            picker.delegate = self
+            picker.allowsEditing = true
+            picker.sourceType = .Camera
+            picker.cameraDevice = .Front
+            picker.cameraCaptureMode = .Photo
+            picker.cameraFlashMode = .Off
+            picker.showsCameraControls = true
+            picker.modalPresentationStyle = .FormSheet
+            presentViewController(picker, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func saveImage(sender:AnyObject) {
+        if let image = imageView.image {
+            UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        }
+    }
+    
+    @IBAction func pickFromCameraRoll(sender: AnyObject) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        picker.sourceType = .PhotoLibrary
+//        picker.modalPresentationStyle = .FullScreen
+        presentViewController(picker, animated: true, completion: nil)
+    }
+    
+    @IBAction func makeNostalgic(sender: AnyObject) {
+        
+        let beginImage = CIImage(image: imageView.image!)
+        if let filter = CIFilter(name: "CISepiaTone") {
+            filter.setValue(beginImage, forKey: kCIInputImageKey)
+            filter.setValue(0.8, forKey: kCIInputIntensityKey)
+            if let outputImage = filter.outputImage {
+                let ciContext = CIContext(options: nil)
+                let cgImageRef = ciContext.createCGImage(outputImage, fromRect:outputImage.extent)
+                let newImage = UIImage(CGImage: cgImageRef)
+                imageView.image = newImage
+            }
+        }
+    }
 }
 
+//MARK: - UIImagePickerControllerDelegate
 extension ViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-  func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
-
-  }
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let image = info[UIImagePickerControllerOriginalImage] {
+            imageView.image = image as? UIImage
+            dismissViewControllerAnimated(true, completion: nil)
+        }
+    }
 }
 
 
